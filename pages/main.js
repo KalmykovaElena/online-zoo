7d9586196e114d1fcff61afdc751c23333914bae
Elena Kalmykova <kalmykhovae@gmail.com>
2022-10-07 22:18:24 +0300

    fix: fixed burger overlay

--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,20 @@
 const burger = document.querySelector('.burger-list')
 
 document.body.addEventListener('click',(e)=>{
+    console.log(e.target)
     if( e.target.classList.contains('burger-close')){
         burger.classList.toggle('burger-open')
-    }else if(e.target.classList.contains('burger-item') || e.target.parentElement.classList.contains('burger-item')){
+        overlayToggle()
+    }else if(e.target.classList.contains('burger-item') || e.target.parentElement.classList.contains('burger-item')
+    || e.target.classList.contains('burger-overlay')){
         burger.classList.remove('burger-open')
+        overlayToggle()
     }
 })
+function overlayToggle(){
+    if( burger.classList.contains('burger-open')){
+        document.querySelector('.burger-overlay').style.opacity='0.5'
+    }else{
+        document.querySelector('.burger-overlay').style.opacity='0'
+    }
+}
